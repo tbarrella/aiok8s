7447a979cb9bbbe6ebcd2cc4060da4cbad6e5d15
Taylor Barrella <tbarrella@gmail.com>
2019-10-05 22:07:26 -0700

    delta fifo: finish tests

--- cache/test_delta_fifo.py
+++ cache/test_delta_fifo.py
@@ -3,7 +3,7 @@ from multiprocessing.pool import ThreadPool
 from queue import Empty, Queue
 from typing import Any, NamedTuple
 
-from .delta_fifo import DeltaFIFO
+from .delta_fifo import DeletedFinalStateUnknown, Delta, DeltaFIFO, Deltas, DeltaType
 from .fifo import RequeueError
 
 
@@ -123,6 +123,218 @@ class TestDeltaFIFO(unittest.TestCase):
             self.assertEqual(test_pop(f).val, expect)
         self.assertEqual(len(f._items), 0)
 
+    def test_add_replace(self):
+        f = DeltaFIFO(test_fifo_object_key_func)
+        f.add(mk_fifo_obj("foo", 10))
+        f.replace([mk_fifo_obj("foo", 15)], "0")
+        got = Queue(maxsize=2)
+
+        def get_popped():
+            while True:
+                got.put(test_pop(f))
+
+        with ThreadPool() as pool:
+            pool.apply_async(get_popped)
+            first = got.get()
+            self.assertEqual(first.val, 15)
+            with self.assertRaises(Empty):
+                got.get(timeout=0.05)
+            self.assertIsNone(f.get(mk_fifo_obj("foo", "")))
+
+    def test_resync_non_existing(self):
+        f = DeltaFIFO(
+            test_fifo_object_key_func,
+            known_objects=KeyLookupFunc(lambda: [mk_fifo_obj("foo", 5)]),
+        )
+        f.delete(mk_fifo_obj("foo", 10))
+        f.resync()
+
+        deltas = f._items["foo"]
+        self.assertEqual(len(deltas), 1)
+        self.assertIs(deltas[0].type, DeltaType.DELETED)
+
+    def test_delete_existing_non_propagated(self):
+        f = DeltaFIFO(
+            test_fifo_object_key_func, known_objects=KeyLookupFunc(lambda: [])
+        )
+        f.add(mk_fifo_obj("foo", 5))
+        f.delete(mk_fifo_obj("foo", 6))
+
+        deltas = f._items["foo"]
+        self.assertEqual(len(deltas), 2)
+        self.assertIs(deltas[-1].type, DeltaType.DELETED)
+
+    def test_replace_makes_deletions(self):
+        f = DeltaFIFO(
+            test_fifo_object_key_func,
+            known_objects=KeyLookupFunc(
+                lambda: [
+                    mk_fifo_obj("foo", 5),
+                    mk_fifo_obj("bar", 6),
+                    mk_fifo_obj("baz", 7),
+                ]
+            ),
+        )
+        f.delete(mk_fifo_obj("baz", 10))
+        f.replace([mk_fifo_obj("foo", 5)], "0")
+
+        expected_list = [
+            Deltas([Delta(DeltaType.DELETED, mk_fifo_obj("baz", 10))]),
+            Deltas([Delta(DeltaType.SYNC, mk_fifo_obj("foo", 5))]),
+            Deltas(
+                [
+                    Delta(
+                        DeltaType.DELETED,
+                        DeletedFinalStateUnknown("bar", mk_fifo_obj("bar", 6)),
+                    )
+                ]
+            ),
+        ]
+        for expected in expected_list:
+            cur = pop(f)
+            self.assertEqual(cur, expected)
+
+    def test_update_resync_race(self):
+        f = DeltaFIFO(
+            test_fifo_object_key_func,
+            known_objects=KeyLookupFunc(lambda: [mk_fifo_obj("foo", 5)]),
+        )
+        f.update(mk_fifo_obj("foo", 6))
+        f.resync()
+
+        expected_list = [Deltas([Delta(DeltaType.UPDATED, mk_fifo_obj("foo", 6))])]
+        for expected in expected_list:
+            cur = pop(f)
+            self.assertEqual(cur, expected)
+
+    def test_has_synced_correct_on_deletion(self):
+        f = DeltaFIFO(
+            test_fifo_object_key_func,
+            known_objects=KeyLookupFunc(
+                lambda: [
+                    mk_fifo_obj("foo", 5),
+                    mk_fifo_obj("bar", 6),
+                    mk_fifo_obj("baz", 7),
+                ]
+            ),
+        )
+        f.replace([mk_fifo_obj("foo", 5)], "0")
+
+        expected_list = [
+            Deltas([Delta(DeltaType.SYNC, mk_fifo_obj("foo", 5))]),
+            Deltas(
+                [
+                    Delta(
+                        DeltaType.DELETED,
+                        DeletedFinalStateUnknown("bar", mk_fifo_obj("bar", 6)),
+                    )
+                ]
+            ),
+            Deltas(
+                [
+                    Delta(
+                        DeltaType.DELETED,
+                        DeletedFinalStateUnknown("baz", mk_fifo_obj("baz", 7)),
+                    )
+                ]
+            ),
+        ]
+        for expected in expected_list:
+            self.assertFalse(f.has_synced())
+            cur = pop(f)
+            self.assertEqual(cur, expected)
+        self.assertTrue(f.has_synced())
+
+    def test_detect_line_jumpers(self):
+        f = DeltaFIFO(test_fifo_object_key_func)
+
+        f.add(mk_fifo_obj("foo", 10))
+        f.add(mk_fifo_obj("bar", 1))
+        f.add(mk_fifo_obj("foo", 11))
+        f.add(mk_fifo_obj("foo", 13))
+        f.add(mk_fifo_obj("zab", 30))
+
+        self.assertEqual(test_pop(f).val, 13)
+
+        f.add(mk_fifo_obj("foo", 14))
+        self.assertEqual(test_pop(f).val, 1)
+        self.assertEqual(test_pop(f).val, 30)
+        self.assertEqual(test_pop(f).val, 14)
+
+    def test_add_if_not_present(self):
+        f = DeltaFIFO(test_fifo_object_key_func)
+
+        f.add(mk_fifo_obj("b", 3))
+        b3 = pop(f)
+        f.add(mk_fifo_obj("c", 4))
+        c4 = pop(f)
+        self.assertEqual(len(f._items), 0)
+
+        f.add(mk_fifo_obj("a", 1))
+        f.add(mk_fifo_obj("b", 2))
+        f.add_if_not_present(b3)
+        f.add_if_not_present(c4)
+
+        self.assertEqual(len(f._items), 3)
+
+        expected_values = [1, 2, 4]
+        for expected in expected_values:
+            self.assertEqual(test_pop(f).val, expected)
+
+    def test_key_of(self):
+        f = DeltaFIFO(test_fifo_object_key_func)
+
+        table = [
+            {"obj": TestFifoObject("A"), "key": "A"},
+            {"obj": DeletedFinalStateUnknown("B", None), "key": "B"},
+            {"obj": TestFifoObject("C"), "key": "C"},
+            {"obj": DeletedFinalStateUnknown("D", None), "key": "D"},
+        ]
+
+        for item in table:
+            got = f.key_of(item["obj"])
+            self.assertEqual(got, item["key"])
+
+    def test_has_synced(self):
+        tests = [
+            {"actions": [], "expected_synced": False},
+            {
+                "actions": [lambda f: f.add(mk_fifo_obj("a", 1))],
+                "expected_synced": True,
+            },
+            {"actions": [lambda f: f.replace([], "0")], "expected_synced": True},
+            {
+                "actions": [
+                    lambda f: f.replace([mk_fifo_obj("a", 1), mk_fifo_obj("b", 2)], "0")
+                ],
+                "expected_synced": False,
+            },
+            {
+                "actions": [
+                    lambda f: f.replace(
+                        [mk_fifo_obj("a", 1), mk_fifo_obj("b", 2)], "0"
+                    ),
+                    pop,
+                ],
+                "expected_synced": False,
+            },
+            {
+                "actions": [
+                    lambda f: f.replace(
+                        [mk_fifo_obj("a", 1), mk_fifo_obj("b", 2)], "0"
+                    ),
+                    pop,
+                    pop,
+                ],
+                "expected_synced": True,
+            },
+        ]
+        for test in tests:
+            f = DeltaFIFO(test_fifo_object_key_func)
+            for action in test["actions"]:
+                action(f)
+            self.assertEqual(f.has_synced(), test["expected_synced"])
+
 
 def pop(queue):
     result = None
@@ -141,7 +353,7 @@ def test_fifo_object_key_func(obj):
 
 class TestFifoObject(NamedTuple):
     name: str
-    val: Any
+    val: Any = None
 
 
 def mk_fifo_obj(name, val):
