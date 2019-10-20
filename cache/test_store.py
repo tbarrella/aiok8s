64d01da8e90292fd7ffbb0fcbd980605a8a65614
Taylor Barrella <tbarrella@gmail.com>
2019-10-19 18:11:34 -0700

    store -> asyncio

--- cache/test_store.py
+++ cache/test_store.py
@@ -1,3 +1,4 @@
+import asyncio
 import unittest
 from typing import NamedTuple
 
@@ -5,47 +6,59 @@ from .index import Indexers
 from .store import new_indexer, new_store
 
 
+def async_test(coro):
+    def wrapper(*args, **kwargs):
+        loop = asyncio.new_event_loop()
+        return loop.run_until_complete(coro(*args, **kwargs))
+
+    return wrapper
+
+
 class TestStore(unittest.TestCase):
-    def test_cache(self):
-        self.do_test_store(new_store(test_store_key_func))
+    @async_test
+    async def test_cache(self):
+        await self.do_test_store(new_store(test_store_key_func))
 
-    def test_index(self):
-        self.do_test_index(new_indexer(test_store_key_func, test_store_indexers()))
+    @async_test
+    async def test_index(self):
+        await self.do_test_index(
+            new_indexer(test_store_key_func, test_store_indexers())
+        )
 
-    def do_test_store(self, store):
+    async def do_test_store(self, store):
         mk_obj = TestStoreObject
 
-        store.add(mk_obj("foo", "bar"))
+        await store.add(mk_obj("foo", "bar"))
         item = store.get(mk_obj("foo", ""))
         self.assertEqual(item.val, "bar")
-        store.update(mk_obj("foo", "baz"))
+        await store.update(mk_obj("foo", "baz"))
         item = store.get(mk_obj("foo", ""))
         self.assertEqual(item.val, "baz")
-        store.delete(mk_obj("foo", ""))
+        await store.delete(mk_obj("foo", ""))
         self.assertIsNone(store.get(mk_obj("foo", "")))
 
-        store.add(mk_obj("a", "b"))
-        store.add(mk_obj("c", "d"))
-        store.add(mk_obj("e", "e"))
+        await store.add(mk_obj("a", "b"))
+        await store.add(mk_obj("c", "d"))
+        await store.add(mk_obj("e", "e"))
         found = {item.val for item in store.list()}
         self.assertGreaterEqual(found, {"b", "d", "e"})
         self.assertEqual(len(found), 3)
 
-        store.replace([mk_obj("foo", "foo"), mk_obj("bar", "bar")], "0")
+        await store.replace([mk_obj("foo", "foo"), mk_obj("bar", "bar")], "0")
         found = {item.val for item in store.list()}
         self.assertGreaterEqual(found, {"foo", "bar"})
         self.assertEqual(len(found), 2)
 
-    def do_test_index(self, indexer):
+    async def do_test_index(self, indexer):
         mk_obj = TestStoreObject
 
         expected = {"b": {"a", "c"}, "f": {"e"}, "h": {"g"}}
-        indexer.add(mk_obj("a", "b"))
-        indexer.add(mk_obj("c", "b"))
-        indexer.add(mk_obj("e", "f"))
-        indexer.add(mk_obj("g", "h"))
+        await indexer.add(mk_obj("a", "b"))
+        await indexer.add(mk_obj("c", "b"))
+        await indexer.add(mk_obj("e", "f"))
+        await indexer.add(mk_obj("g", "h"))
         for k, v in expected.items():
-            index_results = indexer.index("by_val", mk_obj("", k))
+            index_results = await indexer.index("by_val", mk_obj("", k))
             found = {item.id for item in index_results}
             self.assertGreaterEqual(found, v)
 
